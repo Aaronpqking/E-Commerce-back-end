1d3896ffc2cc05e343171225bdf215f85b6cb557
aKingsView <106622740+aKingsView@users.noreply.github.com>
2022-11-13 23:27:04 -0500

    init

---
+++ E-Commerce-back-end/Develop/models/ProductTag.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes, INTEGER } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: INTEGER,
+
+    },
+    tag_id: {
+      type: INTEGER,
+      id
+    }
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
